036a2a7aaeda4702356499718f1a1a871c9af2f4
fyfq <iamtomblack@outlook.com>
2022-01-16 13:22:22 +0800

    Changes

---
+++ HelloWorld/src/MathCal.java
@@ -0,0 +1,9 @@
+public class MathCal {
+    public static void main(String[] args) {
+        System.out.println("5+6");
+        System.out.println(5+6);
+        //注意下列两者的差别
+        System.out.println(5/8);
+        System.out.println(5/8.0);
+    }
+}
